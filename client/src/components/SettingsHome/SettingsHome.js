deb520c46bdf106006a117bdd1b4b0536c545b9b
Bahaa Desoky <bdesoky657@gmail.com>
2022-12-29 01:45:30 -0500

    Added WIP settings page

---
+++ client/src/components/SettingsHome/SettingsHome.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { Grid, Box, Paper, Typography } from "@mui/material";
+
+const SettingsHome = () => {
+  return (
+    <Box sx={{ margin: "auto", width: "80vw" }}>
+      <Typography variant="h4">Settings</Typography>
+      <Typography variant="h5">WIP</Typography>
+    </Box>
+  );
+};
+
+export default SettingsHome;
