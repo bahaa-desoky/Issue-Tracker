5da6c96ea7c06375c51603f1457dae3fedf0c11f
Bahaa Desoky <bdesoky657@gmail.com>
2023-01-04 09:25:40 -0500

    Added RTK Query features (store)

--- client/src/index.js
+++ client/src/index.js
@@ -2,15 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { Provider } from "react-redux";
-import { createStore, applyMiddleware, compose } from "redux";
-import thunk from "redux-thunk";
-import reducers from "./reducers";
+import { store } from "./features/store";
+
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
-
-const store = createStore(reducers, compose(applyMiddleware(thunk)));
 
 ReactDOM.render(
   <Provider store={store}>
