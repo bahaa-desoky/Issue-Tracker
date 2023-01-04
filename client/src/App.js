d31809163733d42244ec34ce99574f12bce480fe
Bahaa Desoky <bdesoky657@gmail.com>
2023-01-04 09:25:14 -0500

    Added Project route (clicking on a specific project from the main Projects page)

--- client/src/App.js
+++ client/src/App.js
@@ -13,6 +13,7 @@ import Sidebar from "./components/Sidebar/Sidebar";
 import { Box } from "@mui/material";
 import ProjectsHome from "./components/ProjectsHome/ProjectsHome";
 import SettingsHome from "./components/SettingsHome/SettingsHome";
+import Project from "./components/Project/Project";
 
 const PrivateRoute = () => {
   const user = JSON.parse(localStorage.getItem("profile"));
@@ -23,13 +24,14 @@ const App = () => {
   return (
     // this makes it so that the hamburger menu for the drawer on mobile appears above the page content
     // that way the page content is centered
-    <Box sx={{ display: { sm: "flex", xs: " " } }}>
+    <Box sx={{ display: { sm: "flex", xs: "" } }}>
       <BrowserRouter>
         <Sidebar />
         <Routes>
           <Route path="/" element={<PrivateRoute />}>
             <Route path="/tickets" element={<TicketsHome />} />
             <Route path="/projects" element={<ProjectsHome />} />
+            <Route path="/projects/:id" element={<Project />} />
             <Route path="/settings" element={<SettingsHome />} />
           </Route>
           <Route path="/auth" element={<Auth />} />
