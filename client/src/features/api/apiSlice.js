979b749bb7c899848f0b3536b5045b0a4cd62806
Bahaa Desoky <bdesoky657@gmail.com>
2023-01-04 17:45:44 -0500

    Changed baseUrl to deployed server url.

--- client/src/features/api/apiSlice.js
+++ client/src/features/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 // Basic authorization. Include credentials with each request or with cookies
 const baseQuery = fetchBaseQuery({
-  baseUrl: "http://localhost:5000/",
+  baseUrl: "https://issue-tracker-anrs.onrender.com/",
   credentials: "include",
   prepareHeaders: (headers) => {
     const token = JSON.parse(localStorage.getItem("profile"))?.token;
